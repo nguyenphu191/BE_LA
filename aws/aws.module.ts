9c6243d43ba8e9376629e1083195ee4943fe9722
nguyenphu191 <149788592+nguyenphu191@users.noreply.github.com>
2025-04-28 18:18:43 +0700

    Delete aws directory

--- aws/aws.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { UploadFileService } from './uploadfile.s3.service';
-
-@Global()
-@Module({
-  providers: [UploadFileService],
-  exports: [UploadFileService],
-})
-export class AwsModule {}
