2e8c0dee0f8453fa7b16aa0273f65cf6992aba98
nguyenphu191 <149788592+nguyenphu191@users.noreply.github.com>
2025-04-28 18:20:07 +0700

    Delete exam_section_items directory

--- exam_section_items/dto/update-exam_section_item.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateExamSectionItemDto } from './create-exam_section_item.dto';
-
-export class UpdateExamSectionItemDto extends PartialType(
-  CreateExamSectionItemDto,
-) {}
